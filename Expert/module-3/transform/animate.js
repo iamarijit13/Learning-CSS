d2e5ca121b6faca2cc135b2438caf77abf1ab99c
ARIJIT <68080361+arispeak@users.noreply.github.com>
2021-05-28 19:26:21 +0530

    tranfromation

---
+++ Expert/module-3/transform/animate.js
@@ -0,0 +1,13 @@
+const box = document.querySelector('.box');
+const play = document.querySelector('#button');
+
+// play.addEventListener('click', () => {
+//     console.log('hello..');
+//     // while(true) {
+//         for( i = 0, j = 1000; i < 1000 && j > 0; i+=20, j-=20) {
+//             console.log(i, j);
+//             box.style.transform = `rotate(${i}deg, ${j}deg, ${i}deg)`;
+            
+//         } 
+//     // }
+// })
